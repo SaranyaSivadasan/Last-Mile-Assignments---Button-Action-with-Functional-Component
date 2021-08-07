eb8b16f039a5cfaa2194fa06457a4f901a358e0d
SaranyaSivadasan <68867201+SaranyaSivadasan@users.noreply.github.com>
2021-08-07 21:10:18 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [para, setPara] = useState("");
+  const [paraId, setParaId] = useState("");
+  const setDetail = () => {
+    setPara(
+      `Hello, I've learnt to use the full-stack evaluation tool. This makes Me so happy`
+    );
+    setParaId("para");
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={setDetail}>
+        RenderPara
+      </button>
+      <p id={paraId}>{para}</p>
     </div>
   );
 }
-
 
 export default App;
